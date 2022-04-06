e2b62afff5a12c4b77367a1d41e1e6156fa3f5c2
Jalen Evans <jalenemmanuelevans@gmail.com>
2022-04-06 18:42:42 -0400

    arm code is somewhat done. idc tho. *Death and taxes*

--- New Deep Space/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ New Deep Space/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,14 +4,68 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {}
+
+  private WPI_TalonSRX liftLeader;
+  private WPI_TalonSRX liftFollower;
+  private static double maxWristAngle = 187;
+  private static double minWristAngle = 90;
+  private static double wobble = 10;
+
+  DigitalInput limitSwitch1;
+  DigitalInput limitSwitch2;
+  DigitalInput limitSwitchStop; 
+
+  private boolean override;
+
+  public ArmSubsystem() {
+    liftLeader = new WPI_TalonSRX(7);
+    liftFollower = new WPI_TalonSRX(8);
+
+    liftFollower.follow(liftLeader);
+
+    override = true;
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void liftUp(double speed) {
+    if (override){
+      liftLeader.set(speed * -1.0);
+    } else {
+      if (limitSwitch1.get()){
+        liftLeader.set( speed * -1.0);
+        holdPosition = getLiftEncoders();
+      } else {
+        liftStop();
+      }
+    }
+  }
+
+  public void liftDown(double speed) {
+    if (override){
+      liftLeader.set(speed);
+    }else {    
+      if (limitSwitch2.get()){
+        liftLeader.set(speed);
+        holdPosition = getLiftEncoders();
+      
+      }else {
+        liftStop();
+      }
+    }
+  }
+
+  public void liftStop() {
+    liftLeader.set(0);
   }
 }
